49f29ead46dc44b95216c7161a717a8291031f3c
Briancarlo24 <Briancarlo2494@gmail.com>
2021-11-30 11:15:36 +0800

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Author - Brian Carlo Birondo - From Zero to Hero Course of Jonas
+
 ///////////////////////////////////////
 // Modal window
 const btnCloseModal = document.querySelector('.btn--close-modal');
